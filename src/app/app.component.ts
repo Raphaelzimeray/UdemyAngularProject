de9c4949bc1618624f38b0d4afd57f253dc25ff2
Raphael Zimeray <raphaelzimeray@me.com>
2024-04-15 20:19:26 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'car-sell';
+  paragraphId = 0;
+
+  text ='Hey guys';
+
+  
 }
